4dc3420b4649c286c78ccbe553cea6b1e598f488
Annie Carmichael <42788203+anniecarm@users.noreply.github.com>
2019-12-12 12:04:49 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	$("#my_image").click(function(){
+		$("#my_image").attr("src","img/jc.png")
+	})
+})
+
+$(document).ready(function(){
+	$("#my_image_1").click(function(){
+		$("#my_image_1").attr("src","img/aco.png")
+	})
+})
+
+$(document).ready(function(){
+	$("#my_image_2").click(function(){
+		$("#my_image_2").attr("src","img/ts.png")
+	})
+})
